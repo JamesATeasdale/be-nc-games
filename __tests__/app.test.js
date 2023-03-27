1e2716da27ca5b487e2cf861594ee992c62601ab
James Teasdale <jamesadamteasdale@gmail.com>
2023-03-27 14:08:58 +0100

    fixed some naming and elaborated on the test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,7 +14,13 @@ describe("200 GET /api/categories should return all categories with the followin
 			.get("/api/categories")
 			.expect(200)
 			.then(({ body }) => {
-				expect(body.rows).toHaveLength(4);
+				expect(body.categories).toHaveLength(4);
+				expect(body.categories).toBeInstanceOf(Array);
+				expect(body.categories).toMatchObject(data.categoryData);
+				expect(body.categories[2]).toHaveProperty(
+					"description",
+					"Games involving physical skill"
+				);
 			});
 	});
 	test("404 GET should return an error", () => {
--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -2,5 +2,5 @@ const { fetchCategories } = require("../models/app.models");
 
 exports.getCategories = async (req, res, next) => {
 	const categories = await fetchCategories();
-	res.status(200).send(categories);
+	res.status(200).send({ categories: categories.rows });
 };
