6c54d08af1378976d6c6517d505d45972fe1a95a
James Teasdale <jamesadamteasdale@gmail.com>
2023-03-27 12:57:00 +0100

    created a get categories function with corresponding tests and errs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const { getCategories } = require("./controllers/app.controller");
+const { notFound } = require("./controllers/errors.controller");
 
 app.use(express.json());
+
+app.get("/api/categories", getCategories);
+
+app.all("/*", notFound);
 
 module.exports = app;
--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,1 +1,6 @@
-const {} = require("../models/app.models");
+const { fetchCategories } = require("../models/app.models");
+
+exports.getCategories = async (req, res, next) => {
+	const categories = await fetchCategories();
+	res.status(200).send(categories);
+};
